3f92b0af68a0e1625e07629a6da899079bd6965d
Artyom Sheludeshev <sheludeshev.artyom@mail.ru>
2018-12-06 10:06:32 +0300

    Now after 3 fails you can click play button and play again

--- game_functions.py
+++ game_functions.py
@@ -37,7 +37,7 @@ def check_keyup_events(event, ship):
   elif event.key == pygame.K_LEFT:
     ship.moving_left = False
 
-def check_events(ai_settings, screen, stats, play_button, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
   """Обрабатывает нажатия клавиш и события мыши"""
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -48,11 +48,21 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
       check_keyup_events(event, ship)
     elif event.type == pygame.MOUSEBUTTONDOWN:
       mouse_x, mouse_y = pygame.mouse.get_pos()
-      check_play_button(stats, play_button, mouse_x, mouse_y)
+      check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
   if play_button.rect.collidepoint(mouse_x, mouse_y):
+    #Сброс статистики
+    stats.reset_stats()
     stats.game_active = True
+
+    #Очистка пришельцев и пуль
+    aliens.empty()
+    bullets.empty()
+
+    #Создание нового флота
+    create_fleet(ai_settings, screen, ship, aliens)
+    ship.center_ship()
 
 def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
   collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
