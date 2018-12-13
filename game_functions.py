6f3693e8cb73b914e0f17354179a716b1829b8d0
Artyom Sheludeshev <sheludeshev.artyom@mail.ru>
2018-12-13 08:58:39 +0300

    Now 3 ships are shown in the left top corner. They are your lives.

--- game_functions.py
+++ game_functions.py
@@ -73,6 +73,7 @@ def start_game(ai_settings, screen, stats, sb, ship, aliens, bullets):
   sb.prep_score()
   sb.prep_high_score()
   sb.prep_level()
+  sb.prep_ships()
 
   #Очистка пришельцев и пуль
   aliens.empty()
@@ -156,9 +157,10 @@ def change_fleet_direction(ai_settings, aliens):
     alien.rect.y += ai_settings.fleet_drop_speed
   ai_settings.fleet_direction *= -1
 
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+def ship_hit(ai_settings, stats, sb, screen, ship, aliens, bullets):
   if stats.ships_left > 1:
     stats.ships_left -= 1
+    sb.prep_ships()
 
     aliens.empty()
     bullets.empty()
@@ -171,21 +173,21 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     stats.game_active = False
     pygame.mouse.set_visible(True)
 
-def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+def check_aliens_bottom(ai_settings, stats, sb, screen, ship, aliens, bullets):
   screen_rect = screen.get_rect()
   for alien in aliens.sprites():
     if alien.rect.bottom >= screen_rect.bottom:
-      ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+      ship_hit(ai_settings, stats, sb, screen, ship, aliens, bullets)
       break
 
 
-def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+def update_aliens(ai_settings, stats, sb, screen, ship, aliens, bullets):
   check_fleet_edges(ai_settings, aliens)
-  check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+  check_aliens_bottom(ai_settings, stats, sb, screen, ship, aliens, bullets)
   aliens.update()
 
   if pygame.sprite.spritecollideany(ship, aliens):
-    ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+    ship_hit(ai_settings, stats, sb, screen, ship, aliens, bullets)
 
 def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
   """Обновляет изображения на кэране и отображает новый экран."""
